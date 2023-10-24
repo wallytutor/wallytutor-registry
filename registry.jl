85c6eaac5cfe25da0cda3645eac5475419b1ad58
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2023-10-24 11:47:19 +0200

    Clean mess

--- registry.jl
+++ registry.jl
@@ -2,28 +2,61 @@
 using Pkg
 Pkg.add("LocalRegistry")
 
+using LibGit2
 using LocalRegistry
 using YAML
 
-data = YAML.load_file("conf.yaml")
-packages = map(p->joinpath(data["reposroot"], p), data["packages"])
 
-@assert all(isdir.(packages))
+"Create a new registry if it does not exist yet."
+function createifnone(
+        name::String,
+        repo::String;
+        path::String
+    )::Nothing
+    registry = joinpath(path, name)
 
-const name = "wallytutor-registry"
-const repo = "https://github.com/wallytutor/wallytutor-registry"
+    if isdir(registry)
+        @info "Found a registry at $(registry)"
+    else
+        create_registry(name, repo)
+    end
 
-try
-    create_registry(
-        name, repo;
-        description = "WallyTutor packages",
-        push        = true
-    )
-catch
+    return nothing
 end
 
-registry = joinpath(homedir(), ".julia", "registries", name)
+"Register a package if not dirty, otherwise a message is issued."
+function saferegister(p::String; registry::String)::Nothing
+    if LibGit2.isdirty(LibGit2.GitRepo(p))
+        @warn "\
+        Target $(p) is dirty!
+        First review and commit changes manually then try again.
+        "
+    else
+        register(p; registry)
+    end
 
-run(Cmd(`git pull`), dir = registry)
+    return nothing
+end
 
-register.(packages; registry = name)
+"Manages workflow of registry creation and packages registering."
+function workflow(
+        name::String,
+        repo::String; 
+        conf::String = "conf.yaml",
+        path::String = joinpath(homedir(), ".julia", "registries")
+    )
+    data = YAML.load_file(conf)
+
+    packages = map(p->joinpath(data["root"], p), data["packages"])
+
+    @assert all(isdir.(packages))
+
+    createifnone(name, repo; path)
+
+    saferegister.(packages; registry = NAME)
+end
+
+workflow(
+    "wallytutor-registry",
+    "https://github.com/wallytutor/wallytutor-registry"
+)
